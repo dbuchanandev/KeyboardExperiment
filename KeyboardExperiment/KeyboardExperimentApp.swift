19c3ad8d746989372f9075cc1a8bbf1070fb30fb
Donavon Buchanan <5569820+dbuchanandev@users.noreply.github.com>
2023-03-30 12:49:16 -0500

    Initial Commit

---
+++ KeyboardExperiment/KeyboardExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardExperimentApp.swift
+//  KeyboardExperiment
+//
+//  Created by Donavon Buchanan on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
